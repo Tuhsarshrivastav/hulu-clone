0f5db78b507945c34cca2f3bc0cdcc58676dd20f
Tuhsarshrivastav <ts3657@gmail.com>
2021-02-21 08:47:20 +0530

    first commit

---
+++ src/Results.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import VideoCard from "./VideoCard";
+import "./Results.css";
+import axios from "./axios";
+import FlipMove from "react-flip-move";
+
+function Results({ selectedOption }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(selectedOption);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [selectedOption]);
+
+  return (
+    <div className="results">
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
+    </div>
+  );
+}
+
+export default Results;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9srpNDYTn-HKmDAG_gOm1UolIKV2Csy4",
+  authDomain: "hulu-clone-fcaec.firebaseapp.com",
+  projectId: "hulu-clone-fcaec",
+  storageBucket: "hulu-clone-fcaec.appspot.com",
+  messagingSenderId: "430699855231",
+  appId: "1:430699855231:web:87f99db2a4c3949c1dae4a",
+  measurementId: "G-DTJDNEV1BX",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
